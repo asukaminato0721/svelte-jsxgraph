1608f1e712b86c4ad33b5fe7dbe63159bdf6f59b
wuyudi <wuyudi119@163.com>
2021-03-05 06:07:54 +0800

    perf: :zap: add onMount

--- src/components/SaveToFile.svelte
+++ src/components/SaveToFile.svelte
@@ -1,5 +1,6 @@
 <script>
   import makeJsToHtml from "../functions/makeJsToHtml.js";
+  import { onMount } from "svelte"; // https://svelte.dev/tutorial/onmount
   import { saveAs } from "file-saver"; // https://github.com/eligrey/FileSaver.js#import-saveas-from-file-saver
   import JSZip from "jszip"; // https://stuk.github.io/jszip/documentation/examples.html
   //https://stackoverflow.com/questions/19059580/client-on-node-js-uncaught-referenceerror-require-is-not-defined
@@ -8,12 +9,21 @@
   $: currentFileName = _currentFileName || "file";
   // https://stackoverflow.com/questions/148441/how-can-i-get-the-content-of-the-file-specified-as-the-src-of-a-script-tag
   // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data#aside_on_promises
-  let jsContent = fetch(
-    "https://cdn.jsdelivr.net/npm/jsxgraph@1.1.0/distrib/jsxgraphcore.js"
-  ).then((response) => response.text());
-  let cssContent = fetch(
-    "https://cdn.jsdelivr.net/npm/jsxgraph@1.1.0/distrib/jsxgraph.css"
-  ).then((response) => response.text());
+  let jsContent;
+  onMount(
+    async () =>
+      (jsContent = fetch(
+        "https://cdn.jsdelivr.net/npm/jsxgraph@1.1.0/distrib/jsxgraphcore.js"
+      ).then((response) => response.text()))
+  );
+
+  let cssContent;
+  onMount(
+    () =>
+      (cssContent = fetch(
+        "https://cdn.jsdelivr.net/npm/jsxgraph@1.1.0/distrib/jsxgraph.css"
+      ).then((response) => response.text()))
+  );
   // https://stuk.github.io/jszip/
   function saveAsHtml() {
     // https://github.com/eligrey/FileSaver.js#saving-text-using-require
