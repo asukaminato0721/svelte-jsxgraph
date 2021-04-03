2d73b343dc9244eb4b4973d42ae7c0260ca31ac0
wuyudi <wuyudi119@163.com>
2021-04-03 13:42:14 +0800

    perf: :fire: remove useless code

--- src/components/Demo.svelte
+++ src/components/Demo.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import InfinitySquare from "../examples/InfinitySquare.js";
-  import Moonwalk from "../examples/Moonwalk.js";
+  import InfinitySquare from "../examples/InfinitySquare";
   let textareaContent =
     "let board = JXG.JSXGraph.initBoard('jxgbox', {boundingbox: [-13, 10, 10, -10], axis:true, keepAspectRatio:true});\n";
   export let currentContent = textareaContent;
@@ -11,12 +10,6 @@
   on:click={() => {
     currentContent = InfinitySquare;
   }}>InfinitySquare</button
->
-<button
-  class="btn btn-primary"
-  on:click={() => {
-    currentContent = Moonwalk;
-  }}>Moonwalk</button
 >
 <button
   class="btn btn-primary"
